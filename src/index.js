0f3fe2c0c6ad06366cccd113855660a82cdb2d37
vrkanaval <veronica22.199@gmail.com>
2023-10-18 19:11:38 +0400

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = []; 
+  const bracketsMap = new Map(bracketsConfig);
+
+  for (const char of str) {
+    if (bracketsMap.has(char)) {
+        if (bracketsMap.get(char) === char && stack[stack.length - 1] === char) {
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    } else {
+        const lastBracket = stack.pop();
+        if (char !== bracketsMap.get(lastBracket)) {
+            return false;
+        }
+    }
+}
+
+return stack.length === 0;
 }
